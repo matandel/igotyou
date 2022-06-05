c3e986f8feaa750f228d7dfcb51baba88ae78af8
Matan Deletic <matandel@protonmail.com>
2022-06-05 16:22:15 +0200

    display version

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core'
 
+ import packageJson from '../../../../package.json'
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
   styleUrls: ['./about.page.scss'],
 })
 export class AboutPage implements OnInit {
+  public version: string = packageJson.version
+
   constructor() {}
 
   ngOnInit() {}
